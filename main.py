ae4dd445e9715e58cca89afa1d5d512e9d2fb30c
Adrian Nowicki <adrian@anowicki.pl>
2024-10-30 08:48:23 +0100

    Add /filter-reports/ endpoint without filtering function

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from enum import Enum
 from functools import lru_cache
-from typing import Annotated
+from typing import Annotated, Optional
 
-from fastapi import APIRouter, Depends, FastAPI
+import requests
+from fastapi import APIRouter, Depends, FastAPI, responses
 from pydantic import BaseModel
 
 from .config import Settings
@@ -19,18 +20,18 @@ class StatusEnum(str, Enum):
 
 
 class Player(BaseModel):
-    id: str
-    firstName: str
-    lastName: str
-    sex: str
-    age: int
+    id: Optional[str]
+    firstName: Optional[str]
+    lastName: Optional[str]
+    sex: Optional[str]
+    age: Optional[int]
 
 
 class Report(BaseModel):
-    id: str
-    name: str
-    status: StatusEnum
-    player: Player
+    id: Optional[str]
+    name: Optional[str]
+    status: Optional[StatusEnum]
+    player: Optional[Player]
 
 
 app = FastAPI()
@@ -40,9 +41,22 @@ router = APIRouter()
 @router.post("/filter-reports/")
 def filter_reports(
     settings: Annotated[Settings, Depends(get_settings)],
-    report: Report,
+    report: Report | None = None,
 ):
-    return {}
+    try:
+        response = requests.get(
+            f"{settings.backend_url}report/all",
+            timeout=0.1,
+        )
+    except (requests.ConnectionError, requests.Timeout) as err:
+        return {
+            "detail": str(err),
+        }
+
+    return responses.Response(
+        content=response.text,
+        media_type="application/json",
+    )
 
 
 app.include_router(router)
