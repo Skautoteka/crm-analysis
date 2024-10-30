d7cda799b1b40ded1528b951063788cb8b1cbf59
Adrian Nowicki <adrian@anowicki.pl>
2024-10-30 08:36:51 +0100

    Add possibility of adding .env file to config

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
     backend_url: str = "http://localhost:3000/api/"
+
+    model_config = SettingsConfigDict(env_file=".env")
