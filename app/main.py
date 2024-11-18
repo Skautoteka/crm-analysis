13007918b0f03852d5b4c00cf0ce197c4d5bae53
Adrian Nowicki <adrian@anowicki.pl>
2024-11-18 16:25:42 +0100

    Add /mean/ endpoint

--- app/main.py
+++ app/main.py
@@ -41,6 +41,19 @@ class Report(BaseModel):
     name: str | None = None
     status: StatusEnum | None = None
     player: Player | None = None
+    playerId: str | None = None
+
+
+class Trait(BaseModel):
+    name: str
+    descending: bool = True
+    minValue: int = 0
+    maxValue: int = 10
+
+
+class Mean(BaseModel):
+    report: Report | None = None
+    trait: Trait
 
 
 app = FastAPI()
@@ -54,7 +67,7 @@ def filter_reports(
 ):
     try:
         response = requests.get(
-            f"{settings.backend_url}report/all",
+            f"{settings.backend_url}report/all-detailed",
             timeout=0.1,
         )
     except (requests.ConnectionError, requests.Timeout) as err:
@@ -85,6 +98,57 @@ def filter_reports(
 
     return responses.Response(
         content=df.filter(query).write_json(),
+        media_type="application/json",
+    )
+
+
+@router.post("/mean/")
+def mean(
+    settings: Annotated[Settings, Depends(get_settings)],
+    mean: Mean,
+):
+    try:
+        response = requests.get(
+            f"{settings.backend_url}report/all-detailed",
+            timeout=0.1,
+        )
+    except (requests.ConnectionError, requests.Timeout) as err:
+        return {
+            "detail": str(err),
+        }
+
+    df = pl.read_json(StringIO(response.text))
+
+    query = pl.col("traits").struct.field("name") == mean.trait.name
+    report = mean.report
+    if report is not None:
+        for k, v in report.model_dump().items():
+            if k == "player":
+                continue
+            if v is None:
+                continue
+            query &= pl.col(k) == v
+        if report.player is not None:
+            for k, v in report.player.model_dump().items():
+                if v is None:
+                    continue
+                query &= pl.col("player").struct.field(k) == v
+
+    content = (
+        df.explode("traits")
+        .filter(query)
+        .group_by("playerId")
+        .agg(pl.col("traits").struct.field("value").mean())
+        .filter(
+            (pl.col("value") >= mean.trait.minValue)
+            & (pl.col("value") < mean.trait.maxValue)
+        )
+        .sort("value", descending=mean.trait.descending)
+        .write_json()
+    )
+
+    return responses.Response(
+        content=content,
         media_type="application/json",
     )
 
--- app/test_main.py
+++ app/test_main.py
@@ -89,3 +89,470 @@ def test_filter_reports():
         )
         assert response.status_code == 200
         assert response.text == damian_report
+
+
+example_reports_to_sort = """
+[
+    {
+        "id": "ba50b02f-def9-4141-9668-156a37be12f6",
+        "name": "a",
+        "status": "IN_PROGRESS",
+        "createdAt": "2024-11-12T20:02:37.000Z",
+        "updatedAt": "2024-11-12T20:02:37.000Z",
+        "playerId": "18228bad-4d09-4773-8aeb-73938a2456c1",
+        "createdById": "9b1e9a92-404c-44e5-bea3-1724d00f1db6",
+        "player": {
+            "name": "Cristiano Ronaldo",
+            "id": "18228bad-4d09-4773-8aeb-73938a2456c1",
+            "firstName": "Cristiano",
+            "lastName": "Ronaldo",
+            "sex": "MALE",
+            "age": 24,
+            "position": "FORWARD",
+            "teamId": null,
+            "createdAt": "2024-11-12T20:00:28.000Z",
+            "updatedAt": "2024-11-12T20:00:28.000Z"
+        },
+        "traits": [
+            {
+                "id": "e0a6b3f5-1234-4f3b-912d-b44a63a1e2b8",
+                "name": "Refleks",
+                "value": 5,
+                "createdAt": "2024-11-12T20:00:28.000Z",
+                "updatedAt": "2024-11-12T20:00:28.000Z",
+                "ReportTrait": {
+                    "traitId": "e0a6b3f5-1234-4f3b-912d-b44a63a1e2b8",
+                    "reportId": "ba50b02f-def9-4141-9668-156a37be12f6",
+                    "createdAt": "2024-11-12T20:02:37.000Z",
+                    "updatedAt": "2024-11-12T20:02:37.000Z"
+                }
+            },
+            {
+                "id": "e0a6b3f5-1234-4f3b-912d-b44a63a1e2b9",
+                "name": "Szybkość",
+                "value": 9,
+                "createdAt": "2024-11-12T20:00:28.000Z",
+                "updatedAt": "2024-11-12T20:00:28.000Z",
+                "ReportTrait": {
+                    "traitId": "e0a6b3f5-1234-4f3b-912d-b44a63a1e2b8",
+                    "reportId": "ba50b02f-def9-4141-9668-156a37be12f6",
+                    "createdAt": "2024-11-12T20:02:37.000Z",
+                    "updatedAt": "2024-11-12T20:02:37.000Z"
+                }
+            }
+        ]
+    },
+    {
+        "id": "ba50b02f-def9-4141-9668-156a37be12f6",
+        "name": "a",
+        "status": "IN_PROGRESS",
+        "createdAt": "2024-11-12T20:02:37.000Z",
+        "updatedAt": "2024-11-12T20:02:37.000Z",
+        "playerId": "18228bad-4d09-4773-8aeb-73938a2456c1",
+        "createdById": "9b1e9a92-404c-44e5-bea3-1724d00f1db6",
+        "player": {
+            "name": "Cristiano Ronaldo",
+            "id": "18228bad-4d09-4773-8aeb-73938a2456c1",
+            "firstName": "Cristiano",
+            "lastName": "Ronaldo",
+            "sex": "MALE",
+            "age": 24,
+            "position": "FORWARD",
+            "teamId": null,
+            "createdAt": "2024-11-12T20:00:28.000Z",
+            "updatedAt": "2024-11-12T20:00:28.000Z"
+        },
+        "traits": [
+            {
+                "id": "e0a6b3f5-1234-4f3b-912d-b44a63a1e2b8",
+                "name": "Refleks",
+                "value": 5,
+                "createdAt": "2024-11-12T20:00:28.000Z",
+                "updatedAt": "2024-11-12T20:00:28.000Z",
+                "ReportTrait": {
+                    "traitId": "e0a6b3f5-1234-4f3b-912d-b44a63a1e2b8",
+                    "reportId": "ba50b02f-def9-4141-9668-156a37be12f6",
+                    "createdAt": "2024-11-12T20:02:37.000Z",
+                    "updatedAt": "2024-11-12T20:02:37.000Z"
+                }
+            },
+            {
+                "id": "e0a6b3f5-1234-4f3b-912d-b44a63a1e2b9",
+                "name": "Szybkość",
+                "value": 9,
+                "createdAt": "2024-11-12T20:00:28.000Z",
+                "updatedAt": "2024-11-12T20:00:28.000Z",
+                "ReportTrait": {
+                    "traitId": "e0a6b3f5-1234-4f3b-912d-b44a63a1e2b8",
+                    "reportId": "ba50b02f-def9-4141-9668-156a37be12f6",
+                    "createdAt": "2024-11-12T20:02:37.000Z",
+                    "updatedAt": "2024-11-12T20:02:37.000Z"
+                }
+            }
+        ]
+    },
+    {
+        "id": "ba50b02f-def9-4141-9668-156a37be12f6",
+        "name": "a",
+        "status": "IN_PROGRESS",
+        "createdAt": "2024-11-12T20:02:37.000Z",
+        "updatedAt": "2024-11-12T20:02:37.000Z",
+        "playerId": "18228bad-4d09-4773-8aeb-73938a2456c1",
+        "createdById": "9b1e9a92-404c-44e5-bea3-1724d00f1db6",
+        "player": {
+            "name": "Cristiano Ronaldo",
+            "id": "18228bad-4d09-4773-8aeb-73938a2456c1",
+            "firstName": "Cristiano",
+            "lastName": "Ronaldo",
+            "sex": "MALE",
+            "age": 24,
+            "position": "FORWARD",
+            "teamId": null,
+            "createdAt": "2024-11-12T20:00:28.000Z",
+            "updatedAt": "2024-11-12T20:00:28.000Z"
+        },
+        "traits": [
+            {
+                "id": "e0a6b3f5-1234-4f3b-912d-b44a63a1e2b8",
+                "name": "Refleks",
+                "value": 5,
+                "createdAt": "2024-11-12T20:00:28.000Z",
+                "updatedAt": "2024-11-12T20:00:28.000Z",
+                "ReportTrait": {
+                    "traitId": "e0a6b3f5-1234-4f3b-912d-b44a63a1e2b8",
+                    "reportId": "ba50b02f-def9-4141-9668-156a37be12f6",
+                    "createdAt": "2024-11-12T20:02:37.000Z",
+                    "updatedAt": "2024-11-12T20:02:37.000Z"
+                }
+            },
+            {
+                "id": "e0a6b3f5-1234-4f3b-912d-b44a63a1e2b9",
+                "name": "Szybkość",
+                "value": 10,
+                "createdAt": "2024-11-12T20:00:28.000Z",
+                "updatedAt": "2024-11-12T20:00:28.000Z",
+                "ReportTrait": {
+                    "traitId": "e0a6b3f5-1234-4f3b-912d-b44a63a1e2b8",
+                    "reportId": "ba50b02f-def9-4141-9668-156a37be12f6",
+                    "createdAt": "2024-11-12T20:02:37.000Z",
+                    "updatedAt": "2024-11-12T20:02:37.000Z"
+                }
+            }
+        ]
+    },
+    {
+        "id": "ba50b02f-def9-4141-9668-156a37be12f7",
+        "name": "a",
+        "status": "IN_PROGRESS",
+        "createdAt": "2024-11-12T20:02:37.000Z",
+        "updatedAt": "2024-11-12T20:02:37.000Z",
+        "playerId": "28228bad-4d09-4773-8aeb-73938a2456c2",
+        "createdById": "9b1e9a92-404c-44e5-bea3-1724d00f1db6",
+        "player": {
+            "name": "Robert Lewandowski",
+            "id": "28228bad-4d09-4773-8aeb-73938a2456c2",
+            "firstName": "Robert",
+            "lastName": "Lewandowski",
+            "sex": "MALE",
+            "age": 36,
+            "position": "FORWARD",
+            "teamId": null,
+            "createdAt": "2024-11-12T20:00:28.000Z",
+            "updatedAt": "2024-11-12T20:00:28.000Z"
+        },
+        "traits": [
+            {
+                "id": "e0a6b3f5-1234-4f3b-912d-b44a63a1e2b8",
+                "name": "Refleks",
+                "value": 6,
+                "createdAt": "2024-11-12T20:00:28.000Z",
+                "updatedAt": "2024-11-12T20:00:28.000Z",
+                "ReportTrait": {
+                    "traitId": "e0a6b3f5-1234-4f3b-912d-b44a63a1e2b8",
+                    "reportId": "ba50b02f-def9-4141-9668-156a37be12f6",
+                    "createdAt": "2024-11-12T20:02:37.000Z",
+                    "updatedAt": "2024-11-12T20:02:37.000Z"
+                }
+            },
+            {
+                "id": "e0a6b3f5-1234-4f3b-912d-b44a63a1e2b9",
+                "name": "Szybkość",
+                "value": 8,
+                "createdAt": "2024-11-12T20:00:28.000Z",
+                "updatedAt": "2024-11-12T20:00:28.000Z",
+                "ReportTrait": {
+                    "traitId": "e0a6b3f5-1234-4f3b-912d-b44a63a1e2b8",
+                    "reportId": "ba50b02f-def9-4141-9668-156a37be12f6",
+                    "createdAt": "2024-11-12T20:02:37.000Z",
+                    "updatedAt": "2024-11-12T20:02:37.000Z"
+                }
+            }
+        ]
+    },
+    {
+        "id": "ba50b02f-def9-4141-9668-156a37be12f7",
+        "name": "a",
+        "status": "IN_PROGRESS",
+        "createdAt": "2024-11-12T20:02:37.000Z",
+        "updatedAt": "2024-11-12T20:02:37.000Z",
+        "playerId": "28228bad-4d09-4773-8aeb-73938a2456c2",
+        "createdById": "9b1e9a92-404c-44e5-bea3-1724d00f1db6",
+        "player": {
+            "name": "Robert Lewandowski",
+            "id": "28228bad-4d09-4773-8aeb-73938a2456c2",
+            "firstName": "Robert",
+            "lastName": "Lewandowski",
+            "sex": "MALE",
+            "age": 36,
+            "position": "FORWARD",
+            "teamId": null,
+            "createdAt": "2024-11-12T20:00:28.000Z",
+            "updatedAt": "2024-11-12T20:00:28.000Z"
+        },
+        "traits": [
+            {
+                "id": "e0a6b3f5-1234-4f3b-912d-b44a63a1e2b8",
+                "name": "Refleks",
+                "value": 6,
+                "createdAt": "2024-11-12T20:00:28.000Z",
+                "updatedAt": "2024-11-12T20:00:28.000Z",
+                "ReportTrait": {
+                    "traitId": "e0a6b3f5-1234-4f3b-912d-b44a63a1e2b8",
+                    "reportId": "ba50b02f-def9-4141-9668-156a37be12f6",
+                    "createdAt": "2024-11-12T20:02:37.000Z",
+                    "updatedAt": "2024-11-12T20:02:37.000Z"
+                }
+            },
+            {
+                "id": "e0a6b3f5-1234-4f3b-912d-b44a63a1e2b9",
+                "name": "Szybkość",
+                "value": 8,
+                "createdAt": "2024-11-12T20:00:28.000Z",
+                "updatedAt": "2024-11-12T20:00:28.000Z",
+                "ReportTrait": {
+                    "traitId": "e0a6b3f5-1234-4f3b-912d-b44a63a1e2b8",
+                    "reportId": "ba50b02f-def9-4141-9668-156a37be12f6",
+                    "createdAt": "2024-11-12T20:02:37.000Z",
+                    "updatedAt": "2024-11-12T20:02:37.000Z"
+                }
+            }
+        ]
+    },
+    {
+        "id": "ba50b02f-def9-4141-9668-156a37be12f7",
+        "name": "a",
+        "status": "IN_PROGRESS",
+        "createdAt": "2024-11-12T20:02:37.000Z",
+        "updatedAt": "2024-11-12T20:02:37.000Z",
+        "playerId": "28228bad-4d09-4773-8aeb-73938a2456c2",
+        "createdById": "9b1e9a92-404c-44e5-bea3-1724d00f1db6",
+        "player": {
+            "name": "Robert Lewandowski",
+            "id": "28228bad-4d09-4773-8aeb-73938a2456c2",
+            "firstName": "Robert",
+            "lastName": "Lewandowski",
+            "sex": "MALE",
+            "age": 36,
+            "position": "FORWARD",
+            "teamId": null,
+            "createdAt": "2024-11-12T20:00:28.000Z",
+            "updatedAt": "2024-11-12T20:00:28.000Z"
+        },
+        "traits": [
+            {
+                "id": "e0a6b3f5-1234-4f3b-912d-b44a63a1e2b8",
+                "name": "Refleks",
+                "value": 6,
+                "createdAt": "2024-11-12T20:00:28.000Z",
+                "updatedAt": "2024-11-12T20:00:28.000Z",
+                "ReportTrait": {
+                    "traitId": "e0a6b3f5-1234-4f3b-912d-b44a63a1e2b8",
+                    "reportId": "ba50b02f-def9-4141-9668-156a37be12f6",
+                    "createdAt": "2024-11-12T20:02:37.000Z",
+                    "updatedAt": "2024-11-12T20:02:37.000Z"
+                }
+            },
+            {
+                "id": "e0a6b3f5-1234-4f3b-912d-b44a63a1e2b9",
+                "name": "Szybkość",
+                "value": 9,
+                "createdAt": "2024-11-12T20:00:28.000Z",
+                "updatedAt": "2024-11-12T20:00:28.000Z",
+                "ReportTrait": {
+                    "traitId": "e0a6b3f5-1234-4f3b-912d-b44a63a1e2b8",
+                    "reportId": "ba50b02f-def9-4141-9668-156a37be12f6",
+                    "createdAt": "2024-11-12T20:02:37.000Z",
+                    "updatedAt": "2024-11-12T20:02:37.000Z"
+                }
+            }
+        ]
+    }
+]
+"""
+
+
+def test_mean():
+    with mock.patch("requests.get") as mocked_get:
+        get = mock.MagicMock()
+        get.text = example_reports_to_sort
+        get.status_code = 200
+        mocked_get.return_value = get
+
+        response = client.post(
+            "/mean/",
+            json={
+                "trait": {
+                    "name": "Szybkość",
+                }
+            },
+        )
+        assert response.status_code == 200
+        assert (
+            response.text
+            == '[{"playerId":"18228bad-4d09-4773-8aeb-73938a2456c1","value":9.333333333333334},{"playerId":"28228bad-4d09-4773-8aeb-73938a2456c2","value":8.333333333333334}]'
+        )
+
+        response = client.post(
+            "/mean/",
+            json={
+                "trait": {
+                    "name": "Szybkość",
+                    "descending": True,
+                }
+            },
+        )
+        assert response.status_code == 200
+        assert (
+            response.text
+            == '[{"playerId":"18228bad-4d09-4773-8aeb-73938a2456c1","value":9.333333333333334},{"playerId":"28228bad-4d09-4773-8aeb-73938a2456c2","value":8.333333333333334}]'
+        )
+
+        response = client.post(
+            "/mean/",
+            json={
+                "trait": {
+                    "name": "Szybkość",
+                    "descending": False,
+                }
+            },
+        )
+        assert response.status_code == 200
+        assert (
+            response.text
+            == '[{"playerId":"28228bad-4d09-4773-8aeb-73938a2456c2","value":8.333333333333334},{"playerId":"18228bad-4d09-4773-8aeb-73938a2456c1","value":9.333333333333334}]'
+        )
+
+        response = client.post(
+            "/mean/",
+            json={
+                "trait": {
+                    "name": "Szybkość",
+                    "minValue": 8,
+                }
+            },
+        )
+        assert response.status_code == 200
+        assert (
+            response.text
+            == '[{"playerId":"18228bad-4d09-4773-8aeb-73938a2456c1","value":9.333333333333334},{"playerId":"28228bad-4d09-4773-8aeb-73938a2456c2","value":8.333333333333334}]'
+        )
+
+        response = client.post(
+            "/mean/",
+            json={
+                "trait": {
+                    "name": "Szybkość",
+                    "minValue": 8,
+                    "maxValue": 10,
+                }
+            },
+        )
+        assert response.status_code == 200
+        assert (
+            response.text
+            == '[{"playerId":"18228bad-4d09-4773-8aeb-73938a2456c1","value":9.333333333333334},{"playerId":"28228bad-4d09-4773-8aeb-73938a2456c2","value":8.333333333333334}]'
+        )
+
+        response = client.post(
+            "/mean/",
+            json={
+                "trait": {
+                    "name": "Szybkość",
+                    "minValue": 9,
+                }
+            },
+        )
+        assert response.status_code == 200
+        assert (
+            response.text
+            == '[{"playerId":"18228bad-4d09-4773-8aeb-73938a2456c1","value":9.333333333333334}]'
+        )
+
+        response = client.post(
+            "/mean/",
+            json={
+                "trait": {
+                    "name": "Szybkość",
+                    "maxValue": 9,
+                }
+            },
+        )
+        assert response.status_code == 200
+        assert (
+            response.text
+            == '[{"playerId":"28228bad-4d09-4773-8aeb-73938a2456c2","value":8.333333333333334}]'
+        )
+
+        response = client.post(
+            "/mean/",
+            json={
+                "trait": {
+                    "name": "Szybkość",
+                    "minValue": 8,
+                    "maxValue": 10,
+                },
+                "report": {
+                    "playerId": "18228bad-4d09-4773-8aeb-73938a2456c1",
+                },
+            },
+        )
+        assert response.status_code == 200
+        assert (
+            response.text
+            == '[{"playerId":"18228bad-4d09-4773-8aeb-73938a2456c1","value":9.333333333333334}]'
+        )
+
+        response = client.post(
+            "/mean/",
+            json={
+                "trait": {
+                    "name": "Szybkość",
+                    "minValue": 8,
+                    "maxValue": 10,
+                },
+                "report": {
+                    "playerId": "28228bad-4d09-4773-8aeb-73938a2456c2",
+                },
+            },
+        )
+        assert response.status_code == 200
+        assert (
+            response.text
+            == '[{"playerId":"28228bad-4d09-4773-8aeb-73938a2456c2","value":8.333333333333334}]'
+        )
+
+        response = client.post(
+            "/mean/",
+            json={
+                "trait": {
+                    "name": "Szybkość",
+                    "minValue": 9,
+                    "maxValue": 10,
+                },
+                "report": {
+                    "playerId": "28228bad-4d09-4773-8aeb-73938a2456c2",
+                },
+            },
+        )
+        assert response.status_code == 200
+        assert response.text == "[]"
